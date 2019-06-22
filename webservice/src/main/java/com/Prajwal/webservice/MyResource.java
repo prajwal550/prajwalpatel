cdded93e6889226217e46ebf46a44eb6af8ba2a7
Prajwal <Prajwal@LAPTOP-KFFHOR3O>
2019-06-22 12:56:50 +0530

    all update

--- webservice/src/main/java/com/Prajwal/webservice/MyResource.java
+++ webservice/src/main/java/com/Prajwal/webservice/MyResource.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Root resource (exposed at "myresource" path)
+ * prajwal
  */
 @Path("myresource")
 public class MyResource {
